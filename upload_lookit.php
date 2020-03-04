32680c23b5ea911170d0b16f4044aae433802b74
Steven Tappert <admin@dark-it.net>
2020-03-05 00:07:37 +0100

    more debugging for lookit uploads

--- upload_lookit.php
+++ upload_lookit.php
@@ -5,7 +5,10 @@ global $httpurl, $db;
 if (!validateAccount()) {
     header('HTTP/1.1 403 Access Denied');
     header('X-Reason: Because u suck!');
+    logMsg('Access Denied: ' . $_SERVER['SERVER_ADDR'] . ' -- ' . print_r($_REQUEST));
     die();
+} else {
+    logMsg("Access Granted: " . getUser());
 }
 
 $uploadFilename = $_FILES['file']['tmp_name'];
